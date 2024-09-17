dea59ccddc3f8baaae2bc586c67da481a3ea614b
eric <songguocheng348@gmail.com>
2024-09-17 11:22:59 +1000

    update

--- Backend/service/apigw/main.go
+++ Backend/service/apigw/main.go
@@ -1,11 +1,27 @@
 package main
 
-import "cloud_distributed_storage/Backend/service/apigw/route"
+import (
+	"cloud_distributed_storage/Backend/service/apigw/handler"
+	"cloud_distributed_storage/Backend/service/apigw/route"
+	"log"
+	"time"
+)
 
 func main() {
+	maxRetries := 5
+	for i := 0; i < maxRetries; i++ {
+		if err := handler.InitService(); err == nil {
+			break
+		}
+		log.Printf("Failed to initialize service, retrying in 5 seconds... (Attempt %d/%d)", i+1, maxRetries)
+		time.Sleep(5 * time.Second)
+	}
+
+	// 初始化路由
 	r := route.Router()
-	err := r.Run(":8081")
-	if err != nil {
-		return
+
+	// 启动 HTTP 服务器
+	if err := r.Run(":8081"); err != nil {
+		log.Fatalf("Failed to run server: %v", err)
 	}
 }
